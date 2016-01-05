1bcec87391a951286eb27595061c2cf8049a15d4
rekirt <rekirt@163.com>
2016-01-05 17:48:30 +0800

    删除第10章重复文件

--- bookdemo/test2/test3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#testing multiple commands in the then section
-testuser=lc
-if grep $testuser /etc/passwd;then 
-   echo The bash file for user $testuser are:
-   ls -a /home/$testuser/.b*
-fi
-
--- bookdemo/test2/test4_1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# testing elif
-if js;then
-    echo "date worked"
-elif ls -al /home 
-then
-    echo "ls -al worked"
-fi
-
