ed686bccb31a52744122de59669bea007942dc65
rekirt <rekirt@163.com>
2016-03-01 13:44:07 +0800

    update

--- bookdemo/chapter26/test4.sh
+++ bookdemo/chapter26/test4.sh
@@ -92,7 +92,7 @@ process_answer
 #
 USER_ACCOUNT_RECORD=$(cat /etc/passwd | grep -w $USER_ACCOUNT)
 
-if [ $? -eq 1] ; then
+if [ $? -eq 1 ] ; then
   echo 
   echo " $USER_ACCOUNT 账户不存在"
   echo "退出脚本"
@@ -102,29 +102,86 @@ if [ $? -eq 1] ; then
 
 fi
 
-#echo 
-#echo "i found this record "
-#echo "$USER_ACCOUNT_RECORD"
-#echo 
-#LINE1="is this the correct user account? [y/n]"
-#get_answer
+echo 
+echo "查找到一条记录"
+echo "$USER_ACCOUNT_RECORD"
+echo 
+LINE1="确定要要删除该账户吗? [y/n]"
+get_answer
 
-
-
-#EXIT_LINE1="because the account. $USER_ACCOUNT . is not "
-#EXIT_LINE2="the one you wish to delete ,we are leaving the script"
-#process_answer
+EXIT_LINE1="取消删除 $USER_ACCOUNT, 退出脚本"
+process_answer
 
 #################
 #查看属于该账户的进程
 #
+echo "$USER_ACCOUNT 的进程:"
+echo 
+ps -u $USER_ACCOUNT
 
-
-
+case $? in 
+  1)
+    echo "当前账户没有正在运行的进程"
+    echo
+  ;;
+  0)
+    unset ANSWER
+    LINE1="是否结束下列进程? [y/n]"
+    get_answer
+    case $ANSWER in 
+      y|Y|YES|yes|Yes|yEs|yeS|YEs|yES)
+        echo
+        trap "rm $USER_ACCOUNT_Running_Process.rpt" SIGTERM SIGINT SIGQUIT
+        ps -u $USER_ACCOUNT > $USER_ACCOUNT_Running_Process.rpt
+        exec < $USER_ACCOUNT_Running_Process.rpt
+        read USER_PROCESS_REC
+        read USER_PROCESS_REC
+        while [ $? -eq 0 ] ; do
+          USER_PID=$(echo $USER_PROCESS_REC | cut -d " " -f1)
+          kill -9 $USER_PID
+          echo "结束进程 $USER_PID"
+          read USER_PROCESS_REC
+        done
+        echo 
+        rm $USER_ACCOUNT_Running_Process.rpt
+      ;;
+      *)
+        echo
+        echo "用户选择退出结束进程"
+        echo 
+      ;;
+    esac
+  ;;
+esac
 #################
 #查找删除账户的所有文件
 #
+echo "查找该账户所有文件"
+REPORT_DATE=`date +%Y%m%d`
+REPORT_FILE=$USER_ACCOUNT"_Files_"$REPORT_DATE".rpt"
+find /home/ -user $USER_ACCOUNT > $REPORT_FILE 2>/dev/null
+
+echo 
+echo "报告生成完毕"
+echo "报告名称: $REPORT_FILE"
+echo "报告位置:		`pwd`"
+
 
 #################
 #删除账户
 #
+LINE1="你确定要将$USER_ACCOUNT从系统中删除吗?[y/s]"
+get_answer
+EXIT_LINE1="撤消了对$USER_ACCOUNT账户的删除操作"
+process_asswer
+userdel $USER_ACCOUNT
+echo 
+echo "账户 $USER_ACCOUNT已经被移除"
+echo
+
+
+
+
+
+
+
