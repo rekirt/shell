e926bc8d2f6026e46036d1e7250d6e03d2e7f70a
rekirt <rekirt@163.com>
2016-03-31 11:46:09 +0800

    vi和vim的高级用法

---
+++ demo/tools/proxy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bash -c 'wget https://raw.githubusercontent.com/racaljk/hosts/master/hosts -qO /tmp/hosts && sudo mv /tmp/hosts /etc/hosts'
---
+++ demo/vim.c
@@ -0,0 +1,54 @@
+this is a new
+this is a new again
+this is a new new again
+
+
+include <studio.h>
+
+int main(){
+  int i ;
+  int factorial -1;
+  int number = 5;
+  for( i =0;i<=number;i++){
+    factorial= factorial*i;
+  }
+  printf("this is a new");
+  return 0;
+}
+
+int add(int x, int y){
+  return x+y;
+}
+
+this new line
+this is a new line, new year, new beginning,new hair,new book,
+ 
+
+
+
+#page2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#page3
+
+
+#this is the last line
